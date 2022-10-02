1a3435f50ba3b568c1a652a9c1516d2fe9336e64
switchpiggy <alanxiao211@gmail.com>
2022-10-01 22:38:21 -0700

    homepage now displays result from making request to firebase db, also deleted /lib/posts.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getAllArticleData  } from '../lib/firebase'
-import { getSortedPostsData  } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
