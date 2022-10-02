56d8ff2137482e3545e60b89a12625c8532f1692
switchpiggy <alanxiao211@gmail.com>
2022-10-01 22:37:01 -0700

    homepage now displays result from making request to firebase db, also deleted /lib/posts.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import { getAllArticleData  } from '../lib/firebase'
+import { getSortedPostsData  } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-export default function Home({ allPostsData }) {
+export default function Home({ allArticleData }) {
   return (
     <Layout home>
       <Head>
@@ -21,15 +22,9 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allArticleData.map(({ id, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+              <h2>{title} - {id}</h2>
             </li>
           ))}
         </ul>
@@ -39,10 +34,10 @@ export default function Home({ allPostsData }) {
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allArticleData = await getAllArticleData()
   return {
     props: {
-      allPostsData
+      allArticleData
     }
   }
 }
